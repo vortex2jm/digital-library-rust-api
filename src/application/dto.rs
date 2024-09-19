92badfee7426f46b1beb27f63b8c05af51bb3ae4
vortex2jm <joaopaulomoura5328@gmail.com>
2024-09-19 17:57:23 -0300

    :recycle: Fixing DTOs

--- src/application/dto.rs
+++ src/application/dto.rs
@@ -1,64 +1,23 @@
 use serde::{Serialize, Deserialize};
-use uuid::Uuid;
+use validator::Validate;
 
-// DTO para representar um livro
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct BookDTO {
-    pub id: Option<Uuid>,       // Autogenerate na criação
+    #[validate(length(min = 1, message = "Title cannot be empty"))]
     pub title: String,
+    
+    #[validate(length(min = 1, message = "Name cannot be empty"))]
     pub author: String,
-    pub loc_for: Option<Uuid>,  // Representa o ID do usuário que alugou
 }
 
-impl BookDTO {
-    // Converte a entidade Book para o DTO
-    pub fn from_entity(book: &crate::domain::book::Book) -> Self {
-        Self {
-            id: Some(book.id),
-            title: book.title.clone(),
-            author: book.author.clone(),
-            loc_for: book.loc_for,
-        }
-    }
-
-    // Converte o DTO para a entidade Book
-    pub fn into_entity(self) -> crate::domain::book::Book {
-        crate::domain::book::Book {
-            id: self.id.unwrap_or_else(Uuid::new_v4),  // Gera ID se não fornecido
-            title: self.title,
-            author: self.author,
-            loc_for: self.loc_for,
-        }
-    }
-}
-
-// DTO para representar um usuário
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct UserDTO {
-    pub id: Option<Uuid>,        // Autogenerate na criação
+    #[validate(length(min = 1, message = "Name cannot be empty"))]
     pub name: String,
+
+    #[validate(email(message = "Invalid email format"))]
+    pub mail: String,
+    
+    #[validate(length(min = 11, max = 11, message = "Invalid cpf format"))]
     pub cpf: String,
-    pub loc_books: Vec<Uuid>,    // Representa os IDs dos livros alugados
-}
-
-impl UserDTO {
-    // Converte a entidade User para o DTO
-    pub fn from_entity(user: &crate::domain::user::User) -> Self {
-        Self {
-            id: Some(user.id),
-            name: user.name.clone(),
-            cpf: user.cpf.clone(),
-            loc_books: user.loc_books.clone(),
-        }
-    }
-
-    // Converte o DTO para a entidade User
-    pub fn into_entity(self) -> crate::domain::user::User {
-        crate::domain::user::User {
-            id: self.id.unwrap_or_else(Uuid::new_v4),  // Gera ID se não fornecido
-            name: self.name,
-            cpf: self.cpf,
-            loc_books: self.loc_books,
-        }
-    }
 }
