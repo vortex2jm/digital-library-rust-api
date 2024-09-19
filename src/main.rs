cbc80a00b06978173d0ec6cc580e40e11f18a237
vortex2jm <joaopaulomoura5328@gmail.com>
2024-09-19 17:58:28 -0300

    :sparkles: Setting up routes and application

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ mod application;
 mod adapters;
 mod domain;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Arc;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+use actix_web::{web, App, HttpServer};
+use adapters::{repositories::book_repository::InMemoryBookRepository, web::routes_config::rt_config};
+use application::book_service::BookService;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let book_repo = Arc::new(InMemoryBookRepository::new());
+    let book_serv = web::Data::new(BookService::new(book_repo));
+
+    HttpServer::new( move || {
         App::new()
-            .service(hello)
+            .app_data(book_serv.clone())
+            .configure(rt_config)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
